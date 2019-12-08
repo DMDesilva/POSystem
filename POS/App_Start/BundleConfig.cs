b4903c720d396bdcba51df93d5388ab853115c70
DMDesilva <danushkads48@gmail.com>
2019-12-08 21:01:47 +0530

    update

--- POS/App_Start/BundleConfig.cs
+++ POS/App_Start/BundleConfig.cs
@@ -10,7 +10,12 @@ namespace POS
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+              "~/Scripts/angular.js",
+              "~/Scripts/angular-material.js",
+              "~/Scripts/angular-animate.js",
+              "~/Scripts/angular-aria.js"));
+            bundles.Add(new ScriptBundle("~/bundles/CustAng").Include("~/ANG_POS_Service/*.js","~/Ang_POS/*.js"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -19,9 +24,8 @@ namespace POS
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/baseCss").Include(
+                      "~/Content/bootstrap.css","~/Content/site.css", "~/Content/angular-material.css", "~/css/font-awesome.css"));
         }
     }
 }
